f1b805cdd82a69e59d07034904487875f523e98e
NayoungJin <soolbe@gmail.com>
2016-10-11 22:05:45 +0900

    add logging

--- src/main/java/me/jin/account/AccountService.java
+++ src/main/java/me/jin/account/AccountService.java
@@ -1,5 +1,6 @@
 package me.jin.account;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.Date;
  */
 @Service
 @Transactional
+@Slf4j
 public class AccountService {
     @Autowired
     private AccountRepository repository;
@@ -19,12 +21,15 @@ public class AccountService {
     @Autowired
     private ModelMapper modelMapper;
 
+    //private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public Account createAccount(AccountDto.Create dto) {
 
         Account account = modelMapper.map(dto, Account.class);
         // TODO 유효한 username인지 판단
         String username = dto.getUsername();
         if (repository.findByUsername(username) != null){
+            log.error("user duplicated exception ~~~~~~~~{}", username);
             throw new UserDuplicatedException(username);
         }
 
